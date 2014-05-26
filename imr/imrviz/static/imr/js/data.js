593f092f9ead729d0cc51ce956854c5a1a09ce8b
Daniele Ciminieri <stonecoldcrazy@gmail.com>
2014-05-26 18:09:57 +0200

    new data

--- imr/imrviz/static/imr/js/data.js
+++ imr/imrviz/static/imr/js/data.js
@@ -3,81 +3,81 @@ var data={
         {
             "name": "world",
             "composite": false,
-            "offer": 900000,
-            "yeld": 0.27,
-            "demand": 2436700,
+            "offer": 277,
+            "yeld": 100,
+            "demand": 277,
             "surface": 510100000,
-            "population": 1000000000,
-            "stock": 4500000,
+            "population": 791000000,
+            "stock": 1179000,
             "exchanges": {
-                "europe": 300,
-                "france": 200,
-                "paris": 100,
-                "business district": 40
+                "europe": 30,
+                "france": 9,
+                "paris": 0,
+                "business_district": 0
             }
         },
         {
             "name": "europe",
             "composite": false,
-            "offer": 100000,
-            "yeld": 0.23,
-            "demand": 620000,
+            "offer": 50,
+            "yeld": 100,
+            "demand": 50,
             "surface":10832312,
-            "population": 139000000,
-            "stock": 900000,
+            "population": 163000000,
+            "stock": 44569,
             "exchanges": {
-                "world": 120,
-                "france": 100,
-                "paris": 50,
-                "business district": 30
+                "world": 3,
+                "france": 10,
+                "paris": 4,
+                "business_district": 0
             }
         },
         {
             "name": "france",
             "composite": false,
-            "offer": 10000,
-            "yeld": 0.23,
-            "demand": 39000,
+            "offer": 6,
+            "yeld": 100,
+            "demand": 6,
             "surface": 675417,
-            "population": 15000000,
-            "stock": 40000,
+            "population": 24500000,
+            "stock": 163,
             "exchanges": {
-                "world": 90,
-                "europe": 60,
-                "paris": 50,
-                "business district": 10
+                "world": 0.1,
+                "europe": 0.2,
+                "paris": 0.4 ,
+                "business_district": 0
             }
         },
         {
             "name": "paris",
             "composite": false,
-            "offer": 460,
-            "yeld": 0.29,
-            "demand": 2000,
+            "offer": 0.2,
+            "yeld": 100,
+            "demand": 0.2,
             "surface": 105.4,
-            "population": 500000,
-            "stock": 2000,
+            "population": 559600,
+            "stock": 0,
             "exchanges": {
-                "world": 30,
-                "europe": 40,
-                "france": 10,
-                "business district": 8
+                "world": 0,
+                "europe": 0,
+                "france": 0,
+                "business_district": 0.01
             }
         },
         {
-            "name": "business district",
+            "name": "business_district",
             "composite": false,
-            "offer": 90,
-            "yeld": 0.29,
-            "demand": 100,
+            "offer": 0.02,
+            "yeld": 100,
+            "demand": 0.02,
             "surface":5,
             "population": 2000,
-            "stock": 800,
+            "stock": 0,
             "exchanges": {
-                "world": 10,
-                "europe": 8,
-                "france": 4,
-                "paris": 2
+                "world": 0,
+                "europe": 0,
+                "france": 0,
+                "paris": 0
             }
         }
     ],
@@ -85,81 +85,81 @@ var data={
         {
             "name": "world",
             "composite": false,
-            "offer": 900000,
-            "yeld": 0.17,
-            "demand": 3436700,
+            "offer": 570,
+            "yeld": 89,
+            "demand": 640,
             "surface": 510100000,
-            "population": 2300000000,
-            "stock": 3500000,
+            "population": 1262000000,
+            "stock": 1175000,
             "exchanges": {
-                "europe": 300,
-                "france": 200,
-                "paris": 100,
-                "business district": 40
+                "europe": 30,
+                "france": 9,
+                "paris": 0,
+                "business_district": 0
             }
         },
         {
             "name": "europe",
             "composite": false,
-            "offer": 100000,
-            "yeld": 0.13,
-            "demand": 820000,
+            "offer": 103,
+            "yeld": 89,
+            "demand": 116,
             "surface":10832312,
-            "population": 439000000,
-            "stock": 700000,
+            "population": 276000000,
+            "stock": 44435,
             "exchanges": {
-                "world": 120,
-                "france": 100,
-                "paris": 50,
-                "business district": 30
+                "world": 3,
+                "france": 10,
+                "paris": 4,
+                "business_district": 0
             }
         },
         {
             "name": "france",
             "composite": false,
-            "offer": 10000,
-            "yeld": 0.13,
-            "demand": 79000,
+            "offer": 13,
+            "yeld": 67,
+            "demand": 14,
             "surface": 675417,
-            "population": 25000000,
-            "stock": 30000,
+            "population": 35783000,
+            "stock": 162,
             "exchanges": {
-                "world": 90,
-                "europe": 60,
-                "paris": 50,
-                "business district": 10
+                "world": 0.1,
+                "europe": 0.2,
+                "paris": 0.4 ,
+                "business_district": 0
             }
         },
         {
             "name": "paris",
             "composite": false,
-            "offer": 460,
-            "yeld": 0.19,
-            "demand": 5000,
+            "offer": 0.2,
+            "yeld": 45,
+            "demand": 0.5,
             "surface": 105.4,
-            "population": 700000,
-            "stock": 1800,
+            "population": 1053262,
+            "stock": 0,
             "exchanges": {
-                "world": 30,
-                "europe": 40,
-                "france": 10,
-                "business district": 8
+                "world": 0,
+                "europe": 0,
+                "france": 0,
+                "business_district": 0.01
             }
         },
         {
-            "name": "business district",
+            "name": "business_district",
             "composite": false,
-            "offer": 90,
-            "yeld": 0.19,
-            "demand": 100,
+            "offer": 0.02,
+            "yeld": 66,
+            "demand": 0.03,
             "surface":5,
             "population": 1000,
-            "stock": 600,
+            "stock": 0,
             "exchanges": {
-                "world": 10,
-                "europe": 8,
-                "france": 4,
-                "paris": 2
+                "world": 0,
+                "europe": 0,
+                "france": 0,
+                "paris": 0
             }
         }
     ],
@@ -167,81 +167,81 @@ var data={
         {
             "name": "world",
             "composite": false,
-            "offer": 900000,
-            "yeld": 0.09,
-            "demand": 6436700,
+            "offer": 1044,
+            "yeld": 39,
+            "demand": 2650,
             "surface": 510100000,
-            "population": 5000000000,
-            "stock": 2500000,
+            "population": 2521000000,
+            "stock": 1092000,
             "exchanges": {
-                "europe": 300,
-                "france": 200,
-                "paris": 100,
-                "business district": 40
+                "europe": 30,
+                "france": 9,
+                "paris": 0,
+                "business_district": 0
             }
         },
         {
             "name": "europe",
             "composite": false,
-            "offer": 100000,
-            "yeld": 0.06,
-            "demand": 920000,
+            "offer": 29,
+            "yeld": 6,
+            "demand": 479,
             "surface":10832312,
-            "population": 639000000,
-            "stock": 500000,
+            "population": 547000000,
+            "stock": 41267,
             "exchanges": {
-                "world": 120,
-                "france": 100,
-                "paris": 50,
-                "business district": 30
+                "world": 3,
+                "france": 10,
+                "paris": 4,
+                "business_district": 0
             }
         },
         {
             "name": "france",
             "composite": false,
-            "offer": 10000,
-            "yeld": 0.06,
-            "demand": 119000,
+            "offer": 4,
+            "yeld": 6,
+            "demand": 60,
             "surface": 675417,
-            "population": 35000000,
-            "stock": 14000,
+            "population": 41647000,
+            "stock": 151,
             "exchanges": {
-                "world": 90,
-                "europe": 60,
-                "paris": 50,
-                "business district": 10
+                "world": 0.1,
+                "europe": 0.2,
+                "paris": 0.4 ,
+                "business_district": 0
             }
         },
         {
             "name": "paris",
             "composite": false,
-            "offer": 460,
-            "yeld": 0.12,
-            "demand": 38000,
+            "offer": 0.1,
+            "yeld": 5,
+            "demand": 2.9,
             "surface": 105.4,
-            "population": 1500000,
-            "stock": 700,
+            "population": 2780000,
+            "stock": 0,
             "exchanges": {
-                "world": 30,
-                "europe": 40,
-                "france": 10,
-                "business district": 8
+                "world": 0,
+                "europe": 0,
+                "france": 0,
+                "business_district": 0.01
             }
         },
         {
-            "name": "business district",
+            "name": "business_district",
             "composite": false,
-            "offer": 90,
-            "yeld": 0.12,
-            "demand": 1000,
+            "offer": 0.02,
+            "yeld": 10,
+            "demand": 0.05,
             "surface":5,
             "population": 30000,
-            "stock": 400,
+            "stock": 0,
             "exchanges": {
-                "world": 10,
-                "europe": 8,
-                "france": 4,
-                "paris": 2
+                "world": 0,
+                "europe": 0,
+                "france": 0,
+                "paris": 0
             }
         }
     ],
@@ -249,81 +249,81 @@ var data={
         {
             "name": "world",
             "composite": false,
-            "offer": 900000,
-            "yeld": 0.07,
-            "demand": 8436700,
+            "offer": 1585,
+            "yeld": 17,
+            "demand": 9110,
             "surface": 510100000,
-            "population": 6000000000,
-            "stock": 900000,
+            "population": 6051000000,
+            "stock": 863000,
             "exchanges": {
-                "europe": 300,
-                "france": 200,
-                "paris": 100,
-                "business district": 40
+                "europe": 30,
+                "france": 9,
+                "paris": 0,
+                "business_district": 0
             }
         },
         {
             "name": "europe",
             "composite": false,
-            "offer": 100000,
-            "yeld": 0.03,
-            "demand": 1120000,
+            "offer": 101,
+            "yeld": 6,
+            "demand": 1690,
             "surface":10832312,
-            "population": 739000000,
-            "stock": 200000,
+            "population": 731500000,
+            "stock": 32637,
             "exchanges": {
-                "world": 120,
-                "france": 100,
-                "paris": 50,
-                "business district": 30
+                "world": 3,
+                "france": 10,
+                "paris": 4,
+                "business_district": 0
             }
         },
         {
             "name": "france",
             "composite": false,
-            "offer": 10000,
-            "yeld": 0.03,
-            "demand": 139000,
+            "offer": 23,
+            "yeld": 9,
+            "demand": 260,
             "surface": 675417,
-            "population": 45000000,
-            "stock": 9000,
+            "population": 58796000,
+            "stock": 119,
             "exchanges": {
-                "world": 90,
-                "europe": 60,
-                "paris": 50,
-                "business district": 10
+                "world": 0.1,
+                "europe": 0.2,
+                "paris": 0.4 ,
+                "business_district": 0
             }
         },
         {
             "name": "paris",
             "composite": false,
-            "offer": 460,
-            "yeld": 0.09,
-            "demand": 48000,
+            "offer": 0.3,
+            "yeld": 9,
+            "demand": 3.8,
             "surface": 105.4,
-            "population": 2000000,
-            "stock": 300,
+            "population": 2128000,
+            "stock": 0,
             "exchanges": {
-                "world": 30,
-                "europe": 40,
-                "france": 10,
-                "business district": 8
+                "world": 0,
+                "europe": 0,
+                "france": 0,
+                "business_district": 0.01
             }
         },
         {
-            "name": "business district",
+            "name": "business_district",
             "composite": false,
-            "offer": 90,
-            "yeld": 0.09,
-            "demand": 3000,
+            "offer": 0.04,
+            "yeld": 13,
+            "demand": 0.3,
             "surface":5,
-            "population": 150000,
-            "stock": 200,
+            "population": 45000,
+            "stock": 0,
             "exchanges": {
-                "world": 10,
-                "europe": 8,
-                "france": 4,
-                "paris": 2
+                "world": 0,
+                "europe": 0,
+                "france": 0,
+                "paris": 0
             }
         }
     ],
@@ -331,81 +331,81 @@ var data={
         {
             "name": "world",
             "composite": false,
-            "offer": 900000,
-            "yeld": 0.17,
-            "demand": 8536700,
+            "offer": 5750,
+            "yeld": 25,
+            "demand": 23000,
             "surface": 510100000,
-            "population": 9000000000,
-            "stock": 500000,
+            "population": 8909000000,
+            "stock": 238000,
             "exchanges": {
-                "europe": 400,
-                "france": 200,
-                "paris": 300,
-                "business district": 70
+                "europe": 30,
+                "france": 9,
+                "paris": 0,
+                "business_district": 0
             }
         },
         {
             "name": "europe",
             "composite": false,
-            "offer": 200000,
-            "yeld": 0.13,
-            "demand": 1320000,
+            "offer": 368,
+            "yeld": 20,
+            "demand": 1840,
             "surface":10832312,
-            "population": 839000000,
-            "stock": 100000,
+            "population": 628000000,
+            "stock": 9004,
             "exchanges": {
-                "world": 120,
-                "france": 100,
-                "paris": 50,
-                "business district": 30
+                "world": 3,
+                "france": 10,
+                "paris": 4,
+                "business_district": 0
             }
         },
         {
             "name": "france",
             "composite": false,
-            "offer": 10000,
-            "yeld": 0.13,
-            "demand": 169000,
+            "offer": 57,
+            "yeld": 20,
+            "demand": 283,
             "surface": 675417,
-            "population": 75000000,
-            "stock": 4000,
+            "population": 69961000,
+            "stock": 33,
             "exchanges": {
-                "world": 90,
-                "europe": 60,
-                "paris": 50,
-                "business district": 10
+                "world": 0.1,
+                "europe": 0.2,
+                "paris": 0.4 ,
+                "business_district": 0
             }
         },
         {
             "name": "paris",
             "composite": false,
-            "offer": 460,
-            "yeld": 0.19,
-            "demand": 60000,
+            "offer": 0.8,
+            "yeld": 20,
+            "demand": 4.1,
             "surface": 105.4,
-            "population": 4000000,
-            "stock": 200,
+            "population": 2548283,
+            "stock": 0,
             "exchanges": {
-                "world": 30,
-                "europe": 40,
-                "france": 10,
-                "business district": 8
+                "world": 0,
+                "europe": 0,
+                "france": 0,
+                "business_district": 0.01
             }
         },
         {
-            "name": "business district",
+            "name": "business_district",
             "composite": false,
-            "offer": 90,
-            "yeld": 0.19,
-            "demand": 4800,
+            "offer": 0.3,
+            "yeld": 33,
+            "demand": 0.9,
             "surface":5,
-            "population": 190000,
-            "stock": 80,
+            "population": 70000,
+            "stock": 0,
             "exchanges": {
-                "world": 10,
-                "europe": 8,
-                "france": 4,
-                "paris": 2
+                "world": 0,
+                "europe": 0,
+                "france": 0,
+                "paris": 0
             }
         }
     ]
